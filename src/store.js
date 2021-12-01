c42c76012354e86c0260f4d2973e2f3a31c4c1dd
junhyeog <junhyeog@korea.ac.kr>
2021-12-02 06:34:51 +0900

    use DevTools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducer';
 import { print1, print2, print3 } from './exampleAddons/middleware';
 
@@ -11,8 +12,12 @@ if (persistedTodosString) {
 	};
 }
 
-const middlewareEnhancer = applyMiddleware(print1, print2, print3);
+const composedEnhancer = composeWithDevTools(
+	// EXAMPLE: Add whatever middleware you actually want to use here
+	applyMiddleware(print1, print2, print3)
+	// other store enhancers if any
+);
 
-const store = createStore(rootReducer, preloadedState, middlewareEnhancer);
+const store = createStore(rootReducer, preloadedState, composedEnhancer);
 
 export default store;
