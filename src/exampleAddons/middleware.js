9910606844ec781bf519cb900667231991ee8486
junhyeog <junhyeog@korea.ac.kr>
2021-12-01 18:21:17 +0900

    fix prettier config

--- src/exampleAddons/middleware.js
+++ src/exampleAddons/middleware.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-unused-vars */
 export const print1 = (storeAPI) => (next) => (action) => {
-  console.log('1')
-  return next(action)
-}
+	console.log('1');
+	return next(action);
+};
 
 export const print2 = (storeAPI) => (next) => (action) => {
-  console.log('2')
-  return next(action)
-}
+	console.log('2');
+	return next(action);
+};
 
 export const print3 = (storeAPI) => (next) => (action) => {
-  console.log('3')
-  return next(action)
-}
+	console.log('3');
+	return next(action);
+};
